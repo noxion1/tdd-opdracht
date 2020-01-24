97e732a3bd4600242e377e588b16e19c76cf55a3
noxion1 <43441327+noxion1@users.noreply.github.com>
2020-01-24 11:09:10 +0100

    Add files via upload

---
+++ src/ContactsManagerTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ContactsManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void addContact() {
+        System.out.println("HOI");
+    }
+
+    @org.junit.Test
+    public void searchContact() {
+    }
+}
